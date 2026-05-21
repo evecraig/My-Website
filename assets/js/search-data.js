ecec5c6bb9d5fd4483f41fbdbf231ad188fb3540
evecraig <ecraig@hawaii.edu>
2026-05-21 09:17:41 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@c9024033cb27279985321c7fd6442459137a50ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -367,11 +367,11 @@ ninja.data = [{
             window.location.href = "/blog/2015/math/";
           
         },
-      },{id: "post-a-post-with-code",
+      },{id: "post-code",
         
-          title: "a post with code",
+          title: "Code",
         
-        description: "an example of a blog post with some code",
+        description: "",
         section: "Posts",
         handler: () => {
           
